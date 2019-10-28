4d79c2d65a53196359f6e92066b1e5cc211c96b9
61070027 Jirat <61070027@kmitl.ac.th>
2019-10-28 18:13:36 +0700

    1222

--- workout-master/js/man.js
+++ workout-master/js/man.js
@@ -1,11 +1,50 @@
-var dict = [{ "problem": ["text", "อะไรเอย"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+// P4 --------------------------------------------------------------- P4 -------------------------------------------------------------
+var chapterP4_100K = [{ "problem": ["text", "อะไรเอย"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP4_divi = [{ "problem": ["text", "อะไรเอยวะเนี้ย"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP4_measure = [{ "problem": ["text", "ohmyGoD"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP4_money = [{ "problem": ["text", "lol"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP4_multi = [{ "problem": ["text", "กำ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP4_plus = [{ "problem": ["text", "กำพกำกำ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP4_smoney = [{ "problem": ["text", "อะไร"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP4_time = [{ "problem": ["text", "อ่ะ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+// P4 --------------------------------------------------------------- P4 -------------------------------------------------------------
+
+
+// P5 --------------------------------------------------------------- P5 -------------------------------------------------------------
+var chapterP5_angle = [{ "problem": ["text", "อะไรเอย"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP5_circle = [{ "problem": ["text", "อะไรเอยวะเนี้ย"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP5_decimal = [{ "problem": ["text", "ohmyGoD"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP5_fraction = [{ "problem": ["text", "lol"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP5_geometry = [{ "problem": ["text", "กำ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP5_plus = [{ "problem": ["text", "กำพกำกำ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP5_statistic = [{ "problem": ["text", "อะไร"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP5_triangle = [{ "problem": ["text", "อ่ะ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+// P5 --------------------------------------------------------------- P5 -------------------------------------------------------------
+
+
+// P6 --------------------------------------------------------------- P6 -------------------------------------------------------------
+var chapterP6_1= [{ "problem": ["text", "อะไรเอย"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP6_2 = [{ "problem": ["text", "อะไรเอยวะเนี้ย"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP6_3= [{ "problem": ["text", "ohmyGoD"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP6_4 = [{ "problem": ["text", "lol"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP6_5 = [{ "problem": ["text", "กำ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP6_6 = [{ "problem": ["text", "กำพกำกำ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP6_7 = [{ "problem": ["text", "อะไร"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+var chapterP6_8 = [{ "problem": ["text", "อ่ะ"], "choice": ["1. เย้", "2. ย้ง", "3.ย้าก", "4.โย่ว"], "ans": 4 }, { "problem": ["img", "http://www.google.com/intl/en_com/images/logo_plain.png"], "choice": ["1. บากะ", "2. lol", "3.อยากเล่นเกม", "4.5555+"], "ans": 3 }, { "problem": ["text", "อะไรวะ"], "choice": ["1. กำ", "2. โฮโฮ", "3.Dio", "4.jojo"], "ans": 1 }]
+// P6 --------------------------------------------------------------- P6 -------------------------------------------------------------
+
+
+var dict = { "100k":chapterP4_100K,"divi":chapterP4_divi, "measure":chapterP4_measure, "money":chapterP4_money, "multi":chapterP4_multi, "plus":chapterP4_plus, "smoney":chapterP4_smoney, "time":chapterP4_time
+            ,"angle": chapterP5_angle, "circle": chapterP5_circle, "decimal":chapterP5_decimal, "fraction":chapterP5_fraction, "square":chapterP5_square, "statistic":chapterP5_statistic, "triangle":chapterP5_triangle, "geometry":chapterP5_geometry
+            ,"1": chapterP6_1, "2": chapterP6_2, "3":chapterP6_3, "4":chapterP6_4, "5":chapterP6_5, "6":chapterP6_6, "7":chapterP6_7, "8":chapterP6_8};
+
 var clause = 0
 var point = 0
 var id = "ans1";
 var id2 = "ans1";
 var choice = ["id1", "id2", "id3", "id4"];
 function make() {
-    document.getElementById("ch").innerHTML = "ข้อ : "+(clause+1)+" / "+dict.length;
+    document.getElementById("ch").innerHTML = "ข้อ : "+(clause+1)+" / "+dict[chapter].length;
     if (clause != 0) {
         document.getElementById(id).style.backgroundColor = "";
         document.getElementById(id2).style.backgroundColor = "";
@@ -15,40 +54,40 @@ function make() {
         document.getElementById(choice[i]).removeChild(document.getElementById(choice[i]).childNodes[0]);
     }
 
-    if (dict[clause]["problem"][0] == "text") {
+    if (dict[chapter][clause]["problem"][0] == "text") {
         var node = document.createElement("H2");
-        var textnode = document.createTextNode(dict[clause]["problem"][1]);
+        var textnode = document.createTextNode(dict[chapter][clause]["problem"][1]);
         node.appendChild(textnode);
         document.getElementById("problem").appendChild(node);
     }
     else {
         var img = document.createElement("img");
-        img.src = dict[clause]["problem"][1];
+        img.src = dict[chapter][clause]["problem"][1];
         var src = document.getElementById("problem");
         src.appendChild(img);
     }
     for (var i in choice) {
         var node = document.createElement("H2");
-        var textnode = document.createTextNode(dict[clause]["choice"][i]);
+        var textnode = document.createTextNode(dict[chapter][clause]["choice"][i]);
         node.appendChild(textnode);
         document.getElementById(choice[i]).appendChild(node);
     }
 }
 function check(ans) {
 
-    if (ans == dict[clause]["ans"]) {
+    if (ans == dict[chapter][clause]["ans"]) {
         id = "ans" + ans;
         document.getElementById(id).style.backgroundColor = "green";
         point++;
     }
     else {
         id = "ans" + ans;
-        id2 = "ans" + dict[clause]["ans"];
+        id2 = "ans" + dict[chapter][clause]["ans"];
         document.getElementById(id).style.backgroundColor = "#B22222";
         document.getElementById(id2).style.backgroundColor = "#006400";
     }
     clause++;
-    if (clause < dict.length) {
+    if (clause < dict[chapter].length) {
 
         setTimeout(function () { make() }, 1500);
 
