479dfc2e44dbd8321c1d34c81375ad0c67fb53de
Ason <aloha_sonza@hotmail.com>
2019-10-29 23:31:28 +0700

    tital

--- workout-master/exam/exam.php
+++ workout-master/exam/exam.php
@@ -6,7 +6,7 @@ session_start();
 <html lang="en">
 
 <head>
-    <title>Workout &mdash; Website Template by Colorlib</title>
+    <title>คณิตศาสตร์ประถมปลาย &mdash; แบบทดสอบวัดความรู้</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
